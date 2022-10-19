287689f900e1e0fcc399b644418e6c018fa4843a
Filip Jerga <jerga99@gmail.com>
2022-10-19 10:45:16 +0800

    Function Refactor

--- lib/md.ts
+++ lib/md.ts
@@ -4,8 +4,14 @@ import fs from "fs";
 
 const getDir = (path: string) => join(process.cwd(), path);
 
+const BLOG_DIR = getDir("/content/blogs");
+
 const getFileNames = (dir: string): string[] => {
   return fs.readdirSync(dir);
+}
+
+const getBlogFileNames = () => {
+  return getFileNames(BLOG_DIR);
 }
 
 const getItemInPath = (filePath: string): string => {
@@ -13,8 +19,12 @@ const getItemInPath = (filePath: string): string => {
   return fileContent;
 }
 
+const getBlog = (fileName: string) => {
+  const blog = getItemInPath(join(BLOG_DIR, fileName));
+  return blog;
+}
+
 export {
-  getFileNames,
-  getItemInPath,
-  getDir
+  getBlogFileNames,
+  getBlog
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { BlogList } from '../components/blogs';
 import { PortfolioList } from '../components/portfolios';
 import { BaseLayout } from '../components/layouts';
-import { getDir, getFileNames, getItemInPath } from '../lib/md';
+import { getBlogFileNames, getBlog } from '../lib/md';
 import { join } from 'path';
 
 
@@ -36,11 +36,10 @@ const Home: NextPage = () => {
 }
 
 export const getStaticProps: GetStaticProps = () => {
-  const blogDir = getDir("/content/blogs");
-  const blogFileNames = getFileNames(blogDir);
+  const blogFileNames = getBlogFileNames();
 
   blogFileNames.forEach((blogFileName) => {
-    const blogContent = getItemInPath(join(blogDir, blogFileName));
+    const blogContent = getBlog(blogFileName);
     console.log(blogContent);
   })
 
