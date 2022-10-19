081e9912a368eff981b0ae91ed4f6f621dc1da66
Filip Jerga <jerga99@gmail.com>
2022-10-19 10:28:26 +0800

    Get Blog Content

--- lib/md.ts
+++ lib/md.ts
@@ -8,7 +8,13 @@ const getFileNames = (dir: string): string[] => {
   return fs.readdirSync(dir);
 }
 
+const getItemInPath = (filePath: string): string => {
+  const fileContent = fs.readFileSync(filePath, "utf8");
+  return fileContent;
+}
+
 export {
   getFileNames,
+  getItemInPath,
   getDir
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,8 @@ import Link from "next/link";
 import { BlogList } from '../components/blogs';
 import { PortfolioList } from '../components/portfolios';
 import { BaseLayout } from '../components/layouts';
-import { getDir, getFileNames } from '../lib/md';
+import { getDir, getFileNames, getItemInPath } from '../lib/md';
+import { join } from 'path';
 
 
 const Home: NextPage = () => {
@@ -38,7 +39,10 @@ export const getStaticProps: GetStaticProps = () => {
   const blogDir = getDir("/content/blogs");
   const blogFileNames = getFileNames(blogDir);
 
-  console.log(blogFileNames);
+  blogFileNames.forEach((blogFileName) => {
+    const blogContent = getItemInPath(join(blogDir, blogFileName));
+    console.log(blogContent);
+  })
 
   return {
     props: {}
